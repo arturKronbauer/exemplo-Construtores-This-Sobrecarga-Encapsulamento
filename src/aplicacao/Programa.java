880c464b7cf79c690643dfd4fb20810b1e347a3c
Artur Henrique Kronbauer <arturhk@gmail.com>
2023-02-12 12:33:30 -0300

    Projeto com construtor

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -7,19 +7,19 @@ import entidades.Produto;
 public class Programa {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Produto produto = new Produto();
 		System.out.println("Informe os dados do produto: ");
 		System.out.print("Nome: ");
-		produto.nome = sc.nextLine();
+		String nome = sc.nextLine();
 		System.out.print("Preço: ");
-		produto.preco = sc.nextDouble();
+		Double preco = sc.nextDouble();
 		System.out.print("Quantidade em Estoque: ");
-		produto.quantidade = sc.nextInt();
+		int quantidade = sc.nextInt();
+		Produto produto = new Produto(nome, preco, quantidade);
 		System.out.println();
 		System.out.println("Dados do produto: " + produto);
 		System.out.println();
 		System.out.print("Informe a quantidade de produtos a ser adicionada no estoque: ");
-		int quantidade = sc.nextInt();
+		quantidade = sc.nextInt();
 		produto.addProdutos(quantidade);
 		System.out.println();
 		System.out.println("Dados atualizados: " + produto);
--- src/entidades/Produto.java
+++ src/entidades/Produto.java
@@ -1,9 +1,16 @@
 package entidades;
 
 public class Produto {
+	
 	public String nome;
 	public double preco;
 	public int quantidade;
+	
+	public Produto(String nome, double preco, int quantidade) {
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
 
 	public double valorEstoque() {
 		return preco * quantidade;
